50cb48810f26c2ef191255cefb9a1df6a286256c
lucassindra3088 <lucas3088@gmail.com>
2026-03-20 10:25:50 +0000

    Update display_power.h

--- boards/shields/prospector_adapter/include/display_power.h
+++ boards/shields/prospector_adapter/include/display_power.h
@@ -7,3 +7,6 @@
 
 void prospector_display_set_sleeping(bool sleeping);
 bool prospector_display_is_sleeping(void);
+void prospector_brightness_fade_off(void);
+void prospector_brightness_fade_on(uint8_t target);
+uint8_t prospector_brightness_get_current(void);
